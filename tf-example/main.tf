f88c77241811b4c5f0c111e7a74c96ff2d5a93e2
PRINCE1KR <118903165+PRINCE1KR@users.noreply.github.com>
2023-07-25 21:45:33 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,63 +1,42 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_instance" "my_instance" {
+  ami                    = "ami-06b09bfacae1453cb"
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  subnet_id              = aws_subnet.my_subnet.id
+  count                  = 2
+  key_name               = "app-ssh-key"
+  instance_type          = "t2.micro"
+}
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "192.168.1.0/24"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "192.168.1.0/27"
+
+}
+
+resource "aws_security_group" "my_security_group" {
+  vpc_id      = aws_vpc.my_vpc.id
+  description = "allowing ssh and http traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-  owners = ["099720109477"] # Canonical
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-# Add a variable to control EC2 instance creation
-variable "create_instance" {
-  description = "Whether to create an EC2 instance"
-  type        = bool
-  default     = true
-}
-
-# Create the EC2 instance conditionally based on the value of create_instance variable
-resource "aws_instance" "app_server" {
-  count         = var.create_instance ? 1 : 0
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name      = "app-ssh-key"
-
-  tags = {
-    Name = var.ec2_name
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-# Add a variable to control S3 bucket creation
-variable "create_s3_bucket" {
-  description = "Whether to create an S3 bucket"
-  type        = bool
-  default     = true
-}
-
-# Create the S3 bucket conditionally based on the value of create_s3_bucket variable
-resource "aws_s3_bucket" "my_bucket" {
-  count  = var.create_s3_bucket ? 1 : 0
-  bucket = "my-s3-bk8t"
-  acl    = "private"
-}
-
-# Output the EC2 instance's public IP address and the S3 bucket name
-output "instance_ip_address" {
-  value       = var.create_instance ? aws_instance.app_server[0].public_ip : null
-  description = "Public IP address of the EC2 instance (if created)"
-}
-
-output "s3_bucket_name" {
-  value       = var.create_s3_bucket ? aws_s3_bucket.my_bucket[0].id : null
-  description = "Name of the S3 bucket (if created)"
 }
